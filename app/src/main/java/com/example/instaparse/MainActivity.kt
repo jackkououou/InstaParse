b59eba9ba29e7934ffeb769900fcadacee9b224e
jackkououou <jackakouyoumdjian@gmail.com>
2022-03-25 11:26:14 -0700

    Calling from Parse serve enabled

--- app/src/main/java/com/example/instaparse/MainActivity.kt
+++ app/src/main/java/com/example/instaparse/MainActivity.kt
@@ -3,12 +3,39 @@ package com.example.instaparse
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.parse.FindCallback
+import com.parse.ParseException
 import com.parse.ParseObject
+import com.parse.ParseQuery
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        queryPosts()
+    }
 
+    //query for all posts on server
+    fun queryPosts() {
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+
+        query.include(Post.KEY_USER)
+        query.findInBackground(object: FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if(e != null) {
+                    //something went wrong
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+                    if (posts != null) {
+                        for (post in posts) {
+                            Log.i(TAG, "Post: " + post.getDescription() +", username: "+ post.getUser())
+                        }
+                    }
+                }
+            }
+        })
+    }
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
